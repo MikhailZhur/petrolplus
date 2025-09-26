24a5aeb1c6e4ff278b347dbd824f47dba41d6d2f
MikhailZhur <miha99_66@mail.ru>
2025-09-26 11:07:23 +0500

    Added Configuration.remote

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -2,13 +2,14 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
-import org.openqa.selenium.devtools.v137.overlay.model.ContainerQueryContainerHighlightConfig;
 
 public class BaseTest {
 
     @BeforeAll
     public static void setUp(){
         Configuration.baseUrl = "https://www.petrolplus.ru/";
+        Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 }
